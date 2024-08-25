9bb9c0f6eed3cb378fe330a0d70cfb9ba0717fcf
FankFerd <155588609+FankFerd1988@users.noreply.github.com>
2024-08-26 02:11:16 +0700

    Add files via upload

---
+++ phpMyAdmin-config-localhost.php
@@ -0,0 +1,14 @@
+/* phpMyAdmin configuration snippet */
+
+/* Paste it to your config.inc.php */
+
+$cfg['DefaultConnectionCollation'] = 'utf8_croatian_ci';
+$cfg['Console']['Mode'] = 'show';
+$cfg['ShowDbStructureCharset'] = true;
+$cfg['ShowDbStructureComment'] = true;
+$cfg['ShowDbStructureCreation'] = true;
+$cfg['ShowDbStructureLastUpdate'] = true;
+$cfg['ShowDbStructureLastCheck'] = true;
+$cfg['Server']['hide_db'] = '';
+$cfg['Server']['only_db'] = '';
+$cfg['ThemeDefault'] = 'bootstrap';
